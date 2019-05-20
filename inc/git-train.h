c14db77a27d00bd35507421e236d6c5c96fec29b
Jonathan FORTIN <jfortin@student.42.fr>
2019-05-20 16:39:53 +0200

    [ADD] base of code

--- inc/git-train.h
+++ inc/git-train.h
@@ -6,7 +6,7 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 15:38:29 by jfortin           #+#    #+#             */
-/*   Updated: 2019/05/01 15:39:10 by jfortin          ###   ########.fr       */
+/*   Updated: 2019/05/20 16:21:25 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define GIT_TRAIN_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+# define MIN 1
+# define MAX 100
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 15:37:11 by jfortin           #+#    #+#             */
-/*   Updated: 2019/05/01 15:39:35 by jfortin          ###   ########.fr       */
+/*   Updated: 2019/05/20 16:34:29 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,30 @@
 
 int	main(void)
 {
-	printf("Hello World\n");
-	return (0);
+	int	mysterious_nbr = 0;
+	int	input = 0;
+
+	// generate random number
+
+	srand(time(NULL));
+	mysterious_nbr = (rand() % (MAX - MIN + 1)) + MIN;
+
+	/* game loop */
+
+	do
+	{
+		// ask the number
+		printf("What is the number ? ");
+		scanf("%d", &input);
+
+		// compare the two numbers
+		if (mysterious_nbr > input)
+			printf("It's more !\n\n");
+		else if (mysterious_nbr < input)
+			printf("It's less !\n\n");
+		else
+			printf ("Congratulation, you found the mysterious number !!!\n\n");
+	} while (input != mysterious_nbr);
+	
+	return (EXIT_SUCCESS);
 }
